42d9d415e973ff75ae0df917795ddeab72e011b5
tranguv <vutrangltv@gmail.com>
2020-01-24 08:26:05 +1100

    Fix stsb

--- run_regression.py
+++ run_regression.py
@@ -246,9 +246,12 @@ class StsProcessor(DataProcessor):
             if i == 0:
                 continue
             guid = "%s-%s" % (set_type, tokenization.convert_to_unicode(line[0]))
-            text_a = tokenization.convert_to_unicode(line[-3])
-            text_b = tokenization.convert_to_unicode(line[-2])
-            label = float(line[-1])
+            text_a = tokenization.convert_to_unicode(line[7])
+            text_b = tokenization.convert_to_unicode(line[8])
+            if set_type == "test":
+                label = 0.0
+            else:
+                label = float(line[-1])
             examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
 
