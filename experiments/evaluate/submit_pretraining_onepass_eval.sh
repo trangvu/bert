21dfee69d47dad724ca1a5548d64a975b5253e17
tranguv <vutrangltv@gmail.com>
2020-01-30 09:52:34 +1100

    Fix eval script

--- experiments/evaluate/submit_pretraining_onepass_eval.sh
+++ experiments/evaluate/submit_pretraining_onepass_eval.sh
@@ -2,5 +2,5 @@
 
 RUN_NAME=$1
 for (( index=0; index<=8; index+=1 )); do
-    sbatch --job-name=eval-pre-epochs-$index --mem-per-cpu=20000 submit-job-m3h-P100.sh evaluate_pretraining_onepass.sh $index $RUN_NAME
+    sbatch --job-name=eval-pre-1pass-$index --mem-per-cpu=20000 submit-job-rqtp.sh evaluate_pretraining_onepass.sh $index $RUN_NAME
 done
