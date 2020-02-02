5bfe86676503661b59e4062e9986a7a0c61cf512
tranguv <vutrangltv@gmail.com>
2020-02-03 09:16:09 +1100

    ner source

--- ner/ner_experiments.sh
+++ ner/ner_experiments.sh
@@ -13,3 +13,8 @@
     sbatch --job-name=conll-adv-tgt-10 submit-job-rqtp.sh task_tuning.sh conll-adv-tgt-10-new  /scratch/da33/trang/masked-lm/models/adapt-conll-adv
     sbatch --job-name=conll-entropy-tgt-10 submit-job-rqtp.sh task_tuning.sh conll-entropy-tgt-10-new  /scratch/da33/trang/masked-lm/models/adapt-conll-entropy
     sbatch --job-name=conll-rand-tgt-10 submit-job-rqtp.sh task_tuning.sh conll-rand-tgt-10-new  /scratch/da33/trang/masked-lm/models/adapt-conll-rand
+
+   @conll source
+   sbatch --job-name=conll-adv-tgt-10 submit-job-rqtp.sh task_tuning_source.sh conll-adv-src-10  /scratch/da33/trang/masked-lm/models/adapt-conll-adv
+    sbatch --job-name=conll-entropy-tgt-10 submit-job-rqtp.sh task_tuning_source.sh conll-entropy-src-10  /scratch/da33/trang/masked-lm/models/adapt-conll-entropy
+    sbatch --job-name=conll-rand-tgt-10 submit-job-rqtp.sh task_tuning_source.sh conll-rand-src-10  /scratch/da33/trang/masked-lm/models/adapt-conll-rand
