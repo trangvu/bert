8c0462f60d5342d5d023f743e063ab18b5f138eb
tranguv <vutrangltv@gmail.com>
2020-03-13 17:45:14 +1100

    update exp

--- experiments/exp_scripts/glue-adapt.sh
+++ experiments/exp_scripts/glue-adapt.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+### adapt rand
+    sbatch --job-name=rand-50 submit-job-m3g-V100.sh adapt-rand.sh 50_50
+    sbatch --job-name=rand-75 submit-job-m3g-V100.sh adapt-rand.sh 75_25
+    sbatch --job-name=rand-100 submit-job-m3g-V100.sh adapt-rand.sh 100_0
